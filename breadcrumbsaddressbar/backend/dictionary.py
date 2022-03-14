4d0f1df273fb26fdff229709e2f02eea66aece2a
Makarov Andrey <winandfx@gmail.com>
2022-03-14 12:39:43 +0300

    Implemeted data model for Dictionary provider

--- breadcrumbsaddressbar/backend/dictionary.py
+++ breadcrumbsaddressbar/backend/dictionary.py
@@ -1,17 +1,17 @@
 import os
+from pathlib import Path
 
-from qtpy import QtCore, QtGui
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtCore import Qt
 
 
 class Dictionary:
-    def __init__(self, data):
-        self.data = data
-        self.fs_model = QtCore.QStringListModel()
-        self.fs_model.setPathPrefix = lambda _: None
+    def __init__(self, data: dict):
+        self.fs_model = DataModel(data)
 
     def check_path(self, path):
         parts = str(path).split(os.sep)
-        d = self.data
+        d = self.fs_model.dat
         try:
             for i in parts:
                 d = d[i]
@@ -20,7 +20,7 @@ class Dictionary:
         return path
 
     def get_devices(self):
-        for i in self.data:
+        for i in self.fs_model.dat:
             yield i, i, None
 
     def get_icon(self, path):
@@ -30,4 +30,40 @@ class Dictionary:
         raise NotImplementedError
 
     def init_completer(self, edit_widget):
-        raise NotImplementedError
+        "Init QCompleter to work with filesystem"
+        completer = QtWidgets.QCompleter(edit_widget)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)
+        completer.setModel(self.fs_model)
+        # Optimize performance https://stackoverflow.com/a/33454284/1119602
+        popup = completer.popup()
+        popup.setUniformItemSizes(True)
+        popup.setLayoutMode(QtWidgets.QListView.Batched)
+        edit_widget.setCompleter(completer)
+        edit_widget.textEdited.connect(self.fs_model.setPathPrefix)
+        return completer
+
+
+class DataModel(QtCore.QStringListModel):
+    def __init__(self, data: dict):
+        super().__init__()
+        self.current_path: "Path|None" = None
+        self.dat = data
+
+    def data(self, index, role):
+        "Get names/icons of files"
+        default = super().data(index, role)
+        if role == Qt.DisplayRole:
+            return Path(default).name
+        return default
+
+    def setPathPrefix(self, prefix: str):
+        path = Path(prefix)
+        if not prefix.endswith(os.path.sep):
+            path = path.parent
+        if path == self.current_path:
+            return  # already listed
+        d: dict = self.dat
+        for i in path.parts:
+            d = d[str(i)]
+        self.setStringList((str(path / k) for k in (d.keys() if d else ())))
+        self.current_path = path
