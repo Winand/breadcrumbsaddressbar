f682f6df41da0e388438aae2e0bbad5060678b0c
Andrey Makarov <winandfx@gmail.com>
2025-02-20 23:24:16 +0300

    clean

--- setup.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-import setuptools
-setuptools.setup(
-    name="breadcrumbsaddressbar",
-    version="{d.year}.{d.month}{d.day:02}".format(d=date.today())[2:],
-    packages=["breadcrumbsaddressbar"],
-)
